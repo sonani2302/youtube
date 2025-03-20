0dd3defbde8d70ef91d5bd61192d431e49ec1436
Jaimin sonani <jaiminsonani364@gmail.com>
2025-03-21 01:30:53 +0530

    6.2 - transformer__auth-to-trpc__protected-proc__rate-limiting

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,5 +1,6 @@
 import { db } from '@/db';
 import { users } from '@/db/schema';
+import { ratelimit } from '@/lib/ratelimit';
 import { auth } from '@clerk/nextjs/server';
 import { initTRPC, TRPCError } from '@trpc/server';
 import { eq } from 'drizzle-orm';
@@ -29,6 +30,7 @@ const t = initTRPC.context<Context>().create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
 export const protectedProcedure = t.procedure.use(async function isAuthed(opts) {
   const { ctx } = opts;
 
@@ -44,6 +46,12 @@ export const protectedProcedure = t.procedure.use(async function isAuthed(opts) 
 
   if(!user) {
     throw new TRPCError({ code: "UNAUTHORIZED"});
+  }
+
+  const { success } = await ratelimit.limit(user.id);
+
+  if(!success) {
+    throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
   }
 
   return opts.next({
