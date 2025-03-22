789028127aed5946d8cc3e238a60694dccea9fd0
Jaimin sonani <jaiminsonani364@gmail.com>
2025-03-22 19:38:40 +0530

    10.1 - infiite scrolling with button

--- src/lib/ratelimit.ts
+++ src/lib/ratelimit.ts
@@ -3,5 +3,5 @@ import { redis } from './redis'
 
 export const ratelimit = new Ratelimit({
     redis,
-    limiter: Ratelimit.slidingWindow(2, "10s")
+    limiter: Ratelimit.slidingWindow(100, "10s")
 })
--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -2,9 +2,23 @@
 
 import { trpc } from "@/trpc/client";
 import { DEFAULT_LIMIT } from "@/constans";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { InfiniteScroll } from "@/components/infinite-scroll";
 
 export const VideosSection = () => {
-    const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
+    return(<>
+        <Suspense fallback={<p>Loading...</p>}>
+            <ErrorBoundary fallback={<p>Error...</p>}>
+                <VideosSectionSuspense />
+            </ErrorBoundary>
+        </Suspense>
+    
+    </>)
+}
+
+export const VideosSectionSuspense = () => {
+    const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery({
         limit: DEFAULT_LIMIT,
     }, {
         getNextPageParam: (lastPage) => lastPage.nextCursor,
@@ -13,6 +27,11 @@ export const VideosSection = () => {
     return(<>
         <div>
            {JSON.stringify(data)}
+           <InfiniteScroll 
+                hasNextPage={query.hasNextPage}
+                isFetchingNextPage={query.isFetchingNextPage}
+                fetchNextPage={query.fetchNextPage}
+           />
         </div>
     </>)
 }
