736e8b90a913b830a233b9558ac79a58dd02a577
Jaimin sonani <jaiminsonani364@gmail.com>
2025-03-22 20:29:37 +0530

    11.1 - Added Table for videos instead JSON

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -5,6 +5,8 @@ import { DEFAULT_LIMIT } from "@/constans";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import Link from "next/link";
 
 export const VideosSection = () => {
     return(<>
@@ -18,7 +20,7 @@ export const VideosSection = () => {
 }
 
 export const VideosSectionSuspense = () => {
-    const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery({
+    const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery({
         limit: DEFAULT_LIMIT,
     }, {
         getNextPageParam: (lastPage) => lastPage.nextCursor,
@@ -26,8 +28,42 @@ export const VideosSectionSuspense = () => {
 
     return(<>
         <div>
-           {JSON.stringify(data)}
+            <div className="border-y">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="pl-6 w-[510px]">Video</TableHead>
+                            <TableHead>Visibility</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Date</TableHead>
+                            <TableHead className="text-right">Views</TableHead>
+                            <TableHead className="text-right">Comment</TableHead>
+                            <TableHead className="text-right pr-6">Likes</TableHead>
+                        </TableRow>    
+                    </TableHeader>
+
+                    <TableBody>
+                        {videos.pages.flatMap((page) => page.items).map((video) => (
+                            <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
+                                <TableRow className="cursor-pointer" >
+                                    <TableCell>{video.title}</TableCell>
+                                    <TableCell>Visibility</TableCell>
+                                    <TableCell>Status</TableCell>
+                                    <TableCell>Date</TableCell>
+                                    <TableCell className="text-right">Views</TableCell>
+                                    <TableCell className="text-right">Comment</TableCell>
+                                    <TableCell className="text-right pr-6">Likes</TableCell>
+
+                                </TableRow>
+                            </Link>
+                        ))}
+                    </TableBody>
+                    
+                </Table>
+            </div>
+           
            <InfiniteScroll 
+                isManual
                 hasNextPage={query.hasNextPage}
                 isFetchingNextPage={query.isFetchingNextPage}
                 fetchNextPage={query.fetchNextPage}
--- src/modules/studio/ui/views/studio-view.tsx
+++ src/modules/studio/ui/views/studio-view.tsx
@@ -3,6 +3,10 @@ import { VideosSection } from "../sections/videos-section";
 export const StudioView = () => {
     return(<>
         <div>
+            <div className="fkex flex-col gap-y-6 pt-2.5">
+                <h1 className="text-2xl font-bold">Channel Content</h1>
+                <p className="text-sx text-muted-foreground">Manage your channel content and videos</p>
+            </div>
             <VideosSection />
         </div>
     </>)
