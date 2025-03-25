fc91277573bbea0eb7526c67a2841042734f16be
Jaimin sonani <jaiminsonani364@gmail.com>
2025-03-26 00:36:38 +0530

    14.1 - Added skeletons of video-section

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -11,15 +11,77 @@ import VideoThubnail from "@/modules/videos/ui/components/video-thumbnail";
 import { snakeCaseToTitle } from "@/lib/utils";
 import { format } from "date-fns";
 import { Globe2Icon, LockIcon } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const VideosSection = () => {
     return(<>
-        <Suspense fallback={<p>Loading...</p>}>
+        <Suspense fallback={<VideoSectionSkeleton />} >
             <ErrorBoundary fallback={<p>Error...</p>}>
                 <VideosSectionSuspense />
             </ErrorBoundary>
         </Suspense>
     
+    </>)
+}
+
+const VideoSectionSkeleton = () => {
+    return(<>
+        <div className="border-y">
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="pl-6 w-[510px]">Video</TableHead>
+                        <TableHead>Visibility</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Date</TableHead>
+                        <TableHead className="text-right">Views</TableHead>
+                        <TableHead className="text-right">Comment</TableHead>
+                        <TableHead className="text-right pr-6">Likes</TableHead>
+                    </TableRow>    
+                </TableHeader>
+
+                <TableBody>
+                    {Array.from({ length: 5 }).map( (_, index) => (
+                        <TableRow key={index}>  
+                            <TableCell className="pl-6">
+                                <div className="flex items-center gap-4">
+                                    <Skeleton className="h-20 w-36" />
+                                    <div className="flex flex-col gap-2">
+                                        <Skeleton className="h-4 w-[100px]" />
+                                        <Skeleton className="h-3 w-[150px]" />
+                                    </div>
+                                </div>
+                            </TableCell>
+
+                            <TableCell>
+                                <Skeleton className="h-4 w-20" />
+                            </TableCell>
+
+                            <TableCell>
+                                <Skeleton className="h-4 w-16" />
+                            </TableCell>
+
+                            <TableCell>
+                                <Skeleton className="h-4 w-24" />
+                            </TableCell>
+
+                            <TableCell className="text-right">
+                                <Skeleton className="h-4 w-12 ml-auto" />
+                            </TableCell>
+
+                            <TableCell className="text-right">
+                                <Skeleton className="h-4 w-12 ml-auto" />
+                            </TableCell>
+
+                            <TableCell className="text-right pr-6">
+                                <Skeleton className="h-4 w-12 ml-auto" />
+                            </TableCell>
+
+                        </TableRow>
+                    )) }
+                </TableBody>
+            </Table>
+        </div>
     </>)
 }
 
@@ -40,9 +102,9 @@ export const VideosSectionSuspense = () => {
                             <TableHead>Visibility</TableHead>
                             <TableHead>Status</TableHead>
                             <TableHead>Date</TableHead>
-                            <TableHead className="text-right">Views</TableHead>
-                            <TableHead className="text-right">Comment</TableHead>
-                            <TableHead className="text-right pr-6">Likes</TableHead>
+                            <TableHead className="text-right text-sm">Views</TableHead>
+                            <TableHead className="text-right text-sm">Comment</TableHead>
+                            <TableHead className="text-right text-sm pr-6">Likes</TableHead>
                         </TableRow>    
                     </TableHeader>
 
@@ -50,7 +112,7 @@ export const VideosSectionSuspense = () => {
                         {videos.pages.flatMap((page) => page.items).map((video) => (
                             <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
                                 <TableRow className="cursor-pointer" >
-                                    <TableCell>
+                                    <TableCell className="pl-6">
                                         <div className="flex items-center gap-4">
                                             <div className="relative aspect-video w-36 shrink-0">
                                                 <VideoThubnail 
