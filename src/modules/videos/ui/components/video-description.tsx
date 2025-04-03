cc586a2ac23ccd05d2ff5680d8fb52920d48d4ff
Jaimin sonani <jaiminsonani364@gmail.com>
2025-04-04 00:38:59 +0530

    18.2 - video view page description box

---
+++ src/modules/videos/ui/components/video-description.tsx
@@ -0,0 +1,59 @@
+import { cn } from "@/lib/utils";
+import { ChevronDownIcon, ChevronUpIcon } from "lucide-react";
+import { useState } from "react";
+
+interface VideoDescriptionProps {
+    compactViews: string;
+    expandViews: string;
+    compactDate: string;
+    expandDate: string;
+    description?: string | null;
+} 
+
+export const VideoDescription = ({
+    compactViews,
+    expandViews,
+    compactDate,
+    expandDate,
+    description
+}: VideoDescriptionProps) => {
+    const [isExpanded, setIsExpanded] = useState(true);
+
+    return(<>
+        <div
+            onClick={() => setIsExpanded(current => !current)}
+            className="bg-secondary/50 rounded-xl p-3 cursor-pointer hover:bg-secondary/70 transition"
+        >
+            <div className="flex gap-2 text-sm mb-2">
+                <span className="font-medium">
+                    {isExpanded ? expandViews : compactViews } views
+                </span>
+                <span className="font-medium">
+                    {isExpanded ? expandDate : compactDate }
+                </span>
+            </div>
+
+            <div className="relative">
+                <p
+                    className={cn(
+                        "text-sm whitespace-pre-wrap",
+                        !isExpanded && "line-clamp-2"
+                    )}
+                >
+                    {description || "No description"}
+                </p>
+                <div className="flex items-center gap-1 mt-4 text-sm font-medium">
+                    {isExpanded ? (
+                        <>
+                            Show less <ChevronUpIcon className="size-4" />
+                        </>
+                    ) : (
+                        <>
+                            Show More <ChevronDownIcon className="size-4" />
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    </>)
+}
--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -1,13 +1,36 @@
+import { useMemo } from "react";
 import { VideoGetOneOutput } from "../../types";
+import { VideoDescription } from "./video-description";
 import { VideoMenu } from "./video-menu";
 import { VideoOwner } from "./video-owner";
 import { VideoReactions } from "./video-reactions";
+import { format, formatDistanceToNow } from "date-fns";
 
 interface VideoTopRowProps {
     video: VideoGetOneOutput;
 }
 
 export const VideoTopRow = ({ video }: VideoTopRowProps) => {
+    const compactViews = useMemo(() => {
+        return Intl.NumberFormat("en", {
+            notation: "compact"
+        }).format(1254548);
+    }, []);
+
+    const expandedViews = useMemo(() => {
+        return Intl.NumberFormat("en", {
+            notation: "standard"
+        }).format(1254548);
+    }, []);
+
+    const compactDate = useMemo(()=>{
+        return formatDistanceToNow(video.createdAt, { addSuffix: true });
+    }, [video.createdAt])
+
+    const expandedDate = useMemo(()=>{
+        return format(video.createdAt, "d MMM yyyy");
+    }, [video.createdAt])
+
     return(<>
         <div className="flex flex-col gap-4 mt-4">
             <h1 className="text-xl font-semibold">{video.title}</h1>
@@ -18,6 +41,13 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
                     <VideoMenu videoId={video.id} variant="secondary" onRemove={() => {}} />
                 </div>
             </div>
+            <VideoDescription
+                compactViews={compactViews}
+                expandViews={expandedViews}
+                compactDate={compactDate}
+                expandDate={expandedDate} 
+                description={video.description}
+            />
         </div>
     </>)
 }
