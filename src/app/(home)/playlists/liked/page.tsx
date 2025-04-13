0b80b42436223c57317831b3f7db00a1a67355e8
Jaimin sonani <jaiminsonani364@gmail.com>
2025-04-14 02:10:24 +0530

    38.3 - prefetch pages having "force-dynamic"

--- src/app/(home)/playlists/liked/page.tsx
+++ src/app/(home)/playlists/liked/page.tsx
@@ -2,6 +2,8 @@ import { DEFAULT_LIMIT } from "@/constans";
 import { LikedView } from "@/modules/playlist/ui/views/liked-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
+export const dynamic = "force-dynamic";
+
 const Page = async () => {
     void trpc.playlists.getLiked.prefetchInfinite({ limit: DEFAULT_LIMIT })
 
